39051c8917d2a1dc6c65a42a50f1c3a473dcca5b
Mohamed05896 <125174475+Mohamed05896@users.noreply.github.com>
2025-12-23 22:42:20 +0200

    Update speed.py

--- BrandrdXMusic/utils/inline/speed.py
+++ BrandrdXMusic/utils/inline/speed.py
@@ -6,27 +6,27 @@ def speed_markup(_, chat_id):
         [
             [
                 InlineKeyboardButton(
-                    text="🕒 0.5x",
+                    text="🕒 بــطــيء 0.5x",
                     callback_data=f"SpeedUP {chat_id}|0.5",
                 ),
                 InlineKeyboardButton(
-                    text="🕓 0.75x",
+                    text="🕓 هــادئ 0.75x",
                     callback_data=f"SpeedUP {chat_id}|0.75",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text=_["P_B_4"],
+                    text="⚡ سـرعـة طـبـيـعـيـة 1.0x",
                     callback_data=f"SpeedUP {chat_id}|1.0",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text="🕤 1.5x",
+                    text="🕤 سـريـع 1.5x",
                     callback_data=f"SpeedUP {chat_id}|1.5",
                 ),
                 InlineKeyboardButton(
-                    text="🕛 2.0x",
+                    text="🕛 سـريـع جـداً 2.0x",
                     callback_data=f"SpeedUP {chat_id}|2.0",
                 ),
             ],
